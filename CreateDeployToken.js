d3286c961691d4b7b3b4b4be5fa71e64121218a6
Sharan Kumar <yenugulasharan8019@gmail.com>
2025-01-26 13:45:32 +0530

    updated

--- CreateDeployToken.js
+++ CreateDeployToken.js
@@ -1,9 +1,14 @@
 const algosdk = require("algosdk");
 const fs = require("fs");
 const algoToken = "";
-const algoServer = "https://node.testnet.algoexplorerapi.io";
-const port = "";
+const algoServer = "https://testnet-api.algonode.cloud";
+const port = 443;
 const algoClient = new algosdk.Algodv2(algoToken, algoServer, port);
+algoClient
+  .status()
+  .do()
+  .then((status) => console.log("Node Status: ", status))
+  .catch((err) => console.error("Failed To get Status: ", err));
 
 const accountData = JSON.parse(fs.readFileSync("account.json", "utf-8"));
 
@@ -21,23 +26,43 @@ const tokenParams = {
   clawback: address,
   defaultFrozen: false,
 };
+// {
+//   from: address,
+//   total: tokenParams.total,
+//   decimals: tokenParams.decimals,
+//   defaultFrozen: tokenParams.defaultFrozen,
+//   manager: tokenParams.manager,
+//   reserve: tokenParams.reserve,
+//   freeze: tokenParams.freeze,
+//   clawback: tokenParams.clawback,
+//   unitName: tokenParams.unitName,
+//   assetName: tokenParams.assetName,
+//   suggestedParams: suggestedParams,
+// }
 
 async function createToken() {
   try {
     const suggestedParams = await algoClient.getTransactionParams().do();
-    const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject({
-      from: address,
-      total: tokenParams.total,
-      decimals: tokenParams.decimals,
-      defaultFrozen: tokenParams.defaultFrozen,
-      manager: tokenParams.manager,
-      reserve: tokenParams.reserve,
-      freeze: tokenParams.freeze,
-      clawback: tokenParams.clawback,
-      unitName: tokenParams.unitName,
-      assetName: tokenParams.assetName,
-      suggestedParams: suggestedParams,
-    });
+    console.log("address: ", address);
+    console.log("Token params: ", tokenParams);
+    console.log("Suggested params: ", suggestedParams);
+    const txn = algosdk.makeAssetCreateTxnWithSuggestedParamsFromObject(
+      address,
+      undefined,
+      tokenParams.total,
+      tokenParams.decimals,
+      tokenParams.defaultFrozen,
+      tokenParams.manager,
+      tokenParams.reserve,
+      tokenParams.freeze,
+      tokenParams.clawback,
+      tokenParams.unitName,
+      tokenParams.assetName,
+      undefined,
+      undefined,
+      suggestedParams
+    );
+
     const signedTxn = txn.signTxn(privateKey);
     const txnID = txn.txID().toString();
     const confirmTxn = await algosdk.waitForConfirmation(algoClient, txnID, 4);
